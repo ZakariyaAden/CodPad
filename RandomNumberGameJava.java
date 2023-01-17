38e18f56f689e3ac4b62e267f3323d0502176af5
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-17 11:48:44 -0600

    Create RandomNumberGameJava.java

---
+++ RandomNumberGameJava.java
@@ -0,0 +1,20 @@
+import java. util. Scanner
+
+public class NumberGame{
+    public static void main{
+        System.out.println("Welcome to the Number Game!!!\n");
+
+        Scanner numGuessScanner = new Scanner(System.in);
+        System.out.println("Enter a number(1~10):");
+        int numGuess = numGuessScanner.nextInt();
+
+        int randNumGen[] = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i : randNumGen) {
+            if ( i != numGuess) {
+                System.
+            }
+        }
+
+    }
+}
