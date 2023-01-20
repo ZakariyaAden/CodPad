7e8f996c66c7e3ace781cb05ae8a3d6ad2069751
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-20 12:02:08 -0600

    Still buggy // beta patch 2

--- TempConverter.java
+++ TempConverter.java
@@ -4,6 +4,27 @@ public class TempConvrt {
     public static void main(String[] args) {
         System.out.println("Java Temp Converter");
         Scanner typeOfConv = new Scanner(System.in);
-        System.out.print("What Type of Conversionp?\n[Enter 1 for Fairenheight & Enter 1 for ]");
+        System.out.print("What Type of Conversionp?\n[Enter 1 for Fairenheight to Celsius & Enter 2 for Celsius to Farenheight]:");
+        int convrBino = typeOfConv.nextInt();
+        typeOfConv.nextLine();
+        typeOfConv.remove();
+        boolean convrTypeF = false;
+        boolean convrTypeC = false;
+
+        if (convrBino == 1) {
+            convrTypeF = true;
+            convrTypeC = false;
+        }
+        if (convrBino == 2) {
+            convrTypeF = false;
+            convrTypeC = true;
+        }
+
+        if (convrTypeF) {
+            System.out.println("\nFairenheight to Celsius");
+        }
+        if (convrTypeC) {
+            System.out.println("\nCelsius to Fairenheight");
+        }
     }
 }
