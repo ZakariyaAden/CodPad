ab956186424827641d0f2074c7b770facc9cf32a
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-20 11:35:26 -0600

    update

---
+++ TempConverter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TempConvrt {
+    public static void main(String[] args) {
+        System.out.println("Java Temp Converter");
+        Scanner typeOfConv = new Scanner(System.in);
+        System.out.print("What Type of Conversionp?\n[Enter 1 for Fairenheight & Enter 1 for ]");
+    }
+}
