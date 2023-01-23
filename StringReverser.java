f77933f8e2330d12456b11c495633b555ea478c1
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-23 12:07:27 -0600

    StringReverser v1.0 Full Release

--- StringReverser.java
+++ StringReverser.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class revString {
+public class StringReverser {
     public static void main (String[] args) {
         System.out.println("Java String Reverser\n");
         Scanner strInput = new Scanner(System.in);
-        System.out.print("Input String To Reverse:");
-        String str = strInput.next();
-        int strLength = str.length();
-        char[] arrConvert = new char[strLength];
-        for (int i = 0; i < arrConvert.length; i++) {
-            arrConvert[i] = str.charAt(i);
-        }
-        try {
-            functForReverse(arrConvert);
-        }
-        catch (Exception err) {
-            System.out.println(err);
+        boolean end = false;
+        while(!end == true) {
+            System.out.print("Input String To Reverse(Press Control + C to exit):\n");
+            String str = strInput.nextLine();
+            System.out.println("\nReversed String:" + reverser(str) + "\n");
         }
     }
-    public static void functForReverse(char[] parArr) {
-        for (int i  = 0; i < parArr.length + 1; i++) {
-            for (int j = parArr.length; j > -1; j--) {
-                char revChar = parArr[j]; 
-                parArr[i] = revChar;
-            }
+
+    public static String reverser(String paraStr) {
+        char[] arrForReverser = new char[paraStr.length()];
+        int strIndex = 0;
+        for (int i = paraStr.length() - 1; i >= 0; i--) {
+            arrForReverser[strIndex] = paraStr.charAt(i);
+            strIndex++;
         }
+        String letter = "";
+        for (int j = 0; j < paraStr.length(); j++) {
+            letter = letter + arrForReverser[j];
+        }
+        return letter;
     }
 }
