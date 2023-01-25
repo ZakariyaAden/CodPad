035a5e87da46b47077b33f3979da74cba80b9c44
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-24 21:23:54 -0600

    WeightConverter beta v0.1

---
+++ WeightConverter.java
@@ -0,0 +1,9 @@
+public class WeightConverter {
+    public static void main(String[] args) {
+    print("Java Weight Converter");
+    }
+
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+}
