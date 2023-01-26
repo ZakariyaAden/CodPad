4012d13e491dc8a89808be75720da62058b6d196
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-01-26 10:18:40 -0600

    Weight Converter v0.5

--- WeightConverter.java
+++ WeightConverter.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
+
 public class WeightConverter {
     public static void main(String[] args) {
-    print("Java Weight Converter");
+        System.out.println("Java Weight Converter");
+        Scanner typeOfConv = new Scanner(System.in);
+        System.out.print("What Type of Conversion?\n[Enter 1 for Pounds to Kilograms & Enter 2 for Kilograms to Pounds]:");
+        String convrBino = typeOfConv.nextLine();
+        boolean convrTypeP = false;
+        boolean convrTypeK = false;
+        if (convrBino.equals("1")) {
+            convrTypeF = true;
+            convrTypeC = false;
+        }
+        if (convrBino.equals("2")) {
+            convrTypeF = false;
+            convrTypeC = true;
+        }
+        if (convrTypeF) {
+            System.out.println("\nFairenheight to Celsius");
+            Scanner farToCelraw = new Scanner(System.in);
+            System.out.print("Input Temp in Fairenheight:");
+            int farToConvr = farToCelraw.nextInt();
+            int farConvrFinal = farenheightConvr(farToConvr);
+            System.out.println(farToConvr + " Fairenheight is " + farConvrFinal + " Celsius");
+        }
+        if (convrTypeC) {
+            System.out.println("\nCelsius to Fairenheight");
+            Scanner farToCelraw = new Scanner(System.in);
+            System.out.print("Input Temp in Celsius:");
+            int celToConvr = farToCelraw.nextInt();
+            int celConvrFinal = celsiusConvr(celToConvr);
+            System.out.println(celToConvr + " Celsius is " + celConvrFinal + " Fairenheight");
+        }
+        if (convrTypeF == false && convrTypeC == false) {
+            System.out.println("Incorrect Input");
+            
+        }
+    }
+    public static int farenheightConvr(int far) {
+        int tempConvr = (far - 32) * 5/9;
+        return tempConvr;
+    }
+    public static int celsiusConvr(int cel) {
+        int tempConvr = (cel * 9/5) + 32;
+        return tempConvr;
     }
 
-    public static void print(String msg) {
-        System.out.println(msg);
-    }
 }
