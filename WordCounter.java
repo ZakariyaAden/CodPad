329e6973632884a1e69018ba7546f56b93a33a70
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-02-06 10:14:47 -0600

    WordCounter v0.9 // needs beta testing

--- WordCounter.java
+++ WordCounter.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class WordCounter{
+    public static void main(String[] args){
+        System.out.println("Java Word Counter\n");
+        Scanner paraGraph = new Scanner(System.in);
+        System.out.println("Paste Paragraph Here:\n");
+        String paraGraphForCounting = paraGraph.nextLine();
+        System.out.println("\n\nParagraph length: " + counter(paraGraphForCounting));
+    }
+    public static int counter(String paraMsg){
+        int length = 1;
+        for(int i = 0; i < arr.length;i++){
+            if(paraMsg.charAt(i) == ' '){
+                length++;
+            }
+        }
+        return length;
+    }
+}
