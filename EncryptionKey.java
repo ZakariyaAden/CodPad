cfff014ea29ff9cc5f5bc090c092f3733ff4f52a
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-02-11 15:28:10 -0600

    EncryptionKey v1 // Class to store hash & message

--- EncryptionKey.java
+++ EncryptionKey.java
@@ -2,7 +2,7 @@ public class EncryptionKey {
     int hash;
     String msg;
 
-    EncryptionKey(int hash, String msg, char[] msgArr, int[] hashArr){
+    EncryptionKey(String msg, int hash){
         this.hash = hash;
         this.msg = msg;
     }
