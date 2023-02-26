c8380a43f08aac2701ba1bcf6a6ffa2330d3ad30
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-02-26 17:42:24 -0600

    Flex v1 // Full Release

---
+++ Flex.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.util.Random;
+
+public class Flex extends JFrame{
+    Flex(){
+        ImageIcon logo = new ImageIcon("flex.png");
+        Border border = BorderFactory.createLineBorder(Color.black,5);
+        JLabel label = new JLabel("Zakariya is the best coder in the group!!!");
+        Random randColor = new Random();
+        label.setForeground(new Color(0,0,0));
+        label.setFont(new Font("MV Boli",Font.PLAIN,20));
+        label.setIcon(logo);
+        label.setIconTextGap(15);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setHorizontalAlignment(JLabel.CENTER);        
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setBorder(border);
+        this.setTitle("GUI");
+        this.setSize(420,420);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setIconImage(logo.getImage());
+        this.add(label);
+        int r1 = randColor.nextInt(255);
+        int r2 = randColor.nextInt(255);
+        int r3 = randColor.nextInt(255);
+        this.getContentPane().setBackground(new Color(r1,r2,r3));
+        this.setVisible(true);
+    }
+    public static void main(String[] args){
+        new Flex();
+    }
+}
